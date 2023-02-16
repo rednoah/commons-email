c799f2dfb5502298a8a4648a4d96e1eb807f550d
Reinhard Pointner <rednoah@filebot.net>
2023-02-16 23:10:33 +0700

    Use jakarta.mail.*

--- src/main/java/org/apache/commons/mail/Email.java
+++ src/main/java/org/apache/commons/mail/Email.java
@@ -26,17 +26,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeUtility;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Store;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+import jakarta.mail.internet.MimeUtility;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -597,7 +597,7 @@ public abstract class Email
     /**
      * Supply a mail Session object from a JNDI directory.
      *
-     * @param jndiName name of JNDI resource (javax.mail.Session type), resource
+     * @param jndiName name of JNDI resource (jakarta.mail.Session type), resource
      * if searched in java:comp/env if name does not start with "java:"
      * @throws IllegalArgumentException if the JNDI name is null or empty
      * @throws NamingException if the resource cannot be retrieved from JNDI directory
@@ -672,7 +672,7 @@ public abstract class Email
             {
                 properties.setProperty(EmailConstants.MAIL_PORT, this.sslSmtpPort);
                 properties.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_PORT, this.sslSmtpPort);
-                properties.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_CLASS, "javax.net.ssl.SSLSocketFactory");
+                properties.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_CLASS, "jakarta.net.ssl.SSLSocketFactory");
                 properties.setProperty(EmailConstants.MAIL_SMTP_SOCKET_FACTORY_FALLBACK, "false");
             }
 
@@ -857,7 +857,7 @@ public abstract class Email
      * @param  aCollection collection of {@code InternetAddress} objects.
      * @return An Email.
      * @throws EmailException Indicates an invalid email address.
-     * @see javax.mail.internet.InternetAddress
+     * @see jakarta.mail.internet.InternetAddress
      * @since 1.0
      */
     public Email setTo(final Collection<InternetAddress> aCollection) throws EmailException
@@ -966,7 +966,7 @@ public abstract class Email
      * @param aCollection collection of {@code InternetAddress} objects.
      * @return An Email.
      * @throws EmailException Indicates an invalid email address.
-     * @see javax.mail.internet.InternetAddress
+     * @see jakarta.mail.internet.InternetAddress
      * @since 1.0
      */
     public Email setCc(final Collection<InternetAddress> aCollection) throws EmailException
@@ -1075,7 +1075,7 @@ public abstract class Email
      * @param  aCollection collection of {@code InternetAddress} objects
      * @return An Email.
      * @throws EmailException Indicates an invalid email address
-     * @see javax.mail.internet.InternetAddress
+     * @see jakarta.mail.internet.InternetAddress
      * @since 1.0
      */
     public Email setBcc(final Collection<InternetAddress> aCollection) throws EmailException
@@ -1154,7 +1154,7 @@ public abstract class Email
      * @param   aCollection collection of {@code InternetAddress} objects
      * @return  An Email.
      * @throws EmailException Indicates an invalid email address
-     * @see javax.mail.internet.InternetAddress
+     * @see jakarta.mail.internet.InternetAddress
      * @since 1.1
      */
     public Email setReplyTo(final Collection<InternetAddress> aCollection) throws EmailException
@@ -1785,7 +1785,7 @@ public abstract class Email
      * Sets whether the email is partially send in case of invalid addresses.
      * <p>
      * In case the mail server rejects an address as invalid, the call to {@link #send()}
-     * may throw a {@link javax.mail.SendFailedException}, even if partial send mode is enabled (emails
+     * may throw a {@link jakarta.mail.SendFailedException}, even if partial send mode is enabled (emails
      * to valid addresses will be transmitted). In case the email server does not reject
      * invalid addresses immediately, but return a bounce message, no exception will be thrown
      * by the {@link #send()} method.
